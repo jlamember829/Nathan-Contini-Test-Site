b48a49d38effea3c5195287d6b1e167522ffc12b
jlamember829 <50644539+jlamember829@users.noreply.github.com>
2021-06-21 08:27:44 -0500

    Add files via upload

---
+++ Loader.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
